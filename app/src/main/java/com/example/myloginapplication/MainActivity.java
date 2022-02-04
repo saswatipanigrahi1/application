d49b58b417ce4fcf01e51517f36969abce0063bb
Praveen Chakravarthy <praveenchakravarthy21@gmail.com>
2022-02-04 17:42:03 +0530

    button listener

--- app/src/main/java/com/example/myloginapplication/MainActivity.java
+++ app/src/main/java/com/example/myloginapplication/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.myloginapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    public void init(){
+        login = findViewById(R.id.loginbtn);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "hello", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
