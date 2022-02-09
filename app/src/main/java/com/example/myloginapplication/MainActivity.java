7df85b443328b191198b4dd2d5d70d9d0ce70c3d
saswatipanigrahi1 <6727saswati@gmail.com>
2022-02-09 10:14:22 +0530

    changes in login

--- app/src/main/java/com/example/myloginapplication/MainActivity.java
+++ app/src/main/java/com/example/myloginapplication/MainActivity.java
@@ -3,30 +3,43 @@ package com.example.myloginapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     protected Button login;
+    protected EditText Username;
+    protected EditText password;
+    protected String input;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Username=findViewById(R.id.username);
+        Username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
 
         init();
     }
 
-    public void init(){
+    public void init() {
         login = findViewById(R.id.loginbtn);
-
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "hello", Toast.LENGTH_SHORT).show();
+                input = "Hello " + Username.getText().toString().trim();
+                Toast.makeText(MainActivity.this, input, Toast.LENGTH_LONG).show();
+                // Toast.makeText(MainActivity.this, "Hello", Toast.LENGTH_LONG).show();
+                // Log.v("hello",);
             }
         });
+
+
     }
 }
